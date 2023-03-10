cebb1440ce55220f08917c46839932fd0bd8664b
LucasLee99 <126792973+LucasLee99@users.noreply.github.com>
2023-03-10 22:08:20 +0800

    Update fast-deploy.sh

--- fast-deploy.sh
+++ fast-deploy.sh
@@ -3,9 +3,9 @@ MAIN_URL="http://ghproxy.com/https://github.com/LucasLee99/docker-fast-deploymen
 mkdir wgcloud && cd wgcloud \
 && wget ${MAIN_URL}/docker-compose.yaml  \
 && wget ${MAIN_URL}/Dockerfile  mysql \
-&& wget ${MAIN_URL}/start.sh  
-&& wget  ${MAIN_URL}/stop.sh  
-&& wget ${MAIN_URL}/wgcloud-server-release.jar  
+&& wget ${MAIN_URL}/start.sh  \
+&& wget  ${MAIN_URL}/stop.sh  \
+&& wget ${MAIN_URL}/wgcloud-server-release.jar  \
 && wget ${MAIN_URL}/wgcloud.sql
 
 chmod 777 wgcloud-server-release.jar
